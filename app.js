cf1f3af8b856ff61181bd00d698b2357e39f3f6c
elgaona <elgaona@ucsc.edu>
2019-03-10 16:53:47 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var score, roundScore, activePlayer;
+var score, roundScore, activePlayer, gamePlaying;
 
-score = [0,0]; //scores for both players
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0; 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    if(gamePlaying) {
+            
+    
     //1. Random Number
     var dice = Math.floor(Math.random() * 6 + 1);
     //2. Result
@@ -56,11 +52,15 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
        } else {
            document.querySelector('#current-' + activePlayer).textContent = roundScore = dice + roundScore;
        }
-    console.log(roundScore);
+    }
+
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
+    
+    if(gamePlaying) {
+            
    if(activePlayer == 0) {
        document.querySelector('#score-0').textContent = score[0] = roundScore + score[0];
        roundScore = 0;
@@ -82,17 +82,27 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#name-0').textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('winner');
+    gamePlaying = false;
 } else if(score[1] >= 10){
     document.querySelector('#name-1').textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-0-panel').classList.remove('active');
-}
+    document.querySelector('.player-1-panel').classList.add('winner');
+    gamePlaying = false;
+    }
+    }
+
 });
 
 
 
 
-document.querySelector('.btn-new').addEventListener('click', function() {
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init() {
+    gamePlaying = true;
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('#score-0').textContent = 0;
     document.querySelector('#score-1').textContent = 0; 
@@ -104,5 +114,7 @@ document.querySelector('.btn-new').addEventListener('click', function() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('#name-0').textContent = 'Player 1';
     document.querySelector('#name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
     score = [0,0];
-});
+}
